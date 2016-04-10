4e5c3dd0233a12e9541d930cbfa63c1bab0c7951
Jaz05 <office_05@hotmail.com>
2016-04-10 20:40:07 -0300

    AppClientes

--- appClientes/app/inicio/inicio.js
+++ appClientes/app/inicio/inicio.js
@@ -2,18 +2,18 @@ var inicio = angular.module("backendEcommerceClientes.inicio", [
 	"ui.router"
 ]);
 
-var categoriesList = $("#idPnlCategorias li");
+var categoriesList = $("#idPnlFiltros #idPnlCategorias li");
 
-if (categoriesList.length > 4) {
+if (categoriesList.length > 3) {
     $("#show-more-cats").show(); 
-    for (i = 4; i < categoriesList.length; i++) { 
+    for (i = 3; i < categoriesList.length; i++) { 
         $(categoriesList[i]).hide();
     }
 }
 
 $("#show-more-cats").click(function(e){
     e.preventDefault();
-    for (i = 4; i < categoriesList.length; i++) { 
+    for (i = 3; i < categoriesList.length; i++) { 
         $(categoriesList[i]).toggle();
     }
     $(this).find("i").toggleClass("fa-angle-up fa-angle-down");

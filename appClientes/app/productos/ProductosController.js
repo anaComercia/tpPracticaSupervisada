4f47034f939de44414e7b28d3ab76dc94f316138
anaComercia <ana.comercia@gmail.com>
2016-04-11 22:30:51 -0300

    commit Ana

--- appClientes/app/productos/ProductosController.js
+++ appClientes/app/productos/ProductosController.js
@@ -13,11 +13,26 @@ angular
 ProductosCtrl.$injector = ["$state"];
 
 function ProductosCtrl($state) {
+    var obj = {
+        title: 'CAMPERA MILANO',
+        price: '$600',
+        gender: 'Mujer',
+        size: ['1','2'],
+        color: ['Azul', 'Rosa', 'Blanca'],
+        stock: 3,
+        image: ['img/modulos/prueba2.jpg', 'img/modulos/prueba3.jpg', 'img/modulos/prueba4.jpg']
+  };
+    
     vm = this;
- //   debugger;
-    vm.changeImage = _changeImage
+    
+    vm.prod = obj;
+    vm.mySelect = {};//seleccion de los combos
+    
+    vm.source = vm.prod.image[0];
+    vm.changeImage = _changeImage;
+    
         
-    function _changeImage(e){
-    //    debugger;
+    function _changeImage($event){
+        vm.source = $event.target.src;
     }
 }

95c2cdb359f24c5d840ae990cec997a6d54ab0f8
anaComercia <ana.comercia@gmail.com>
2016-04-16 21:22:24 -0300

    commit Ana
    Se modifica el modulo de PERFIL de usuario

--- appClientes/app/perfil/perfil.js
+++ appClientes/app/perfil/perfil.js
@@ -1,3 +1,9 @@
 var perfil = angular.module("backendEcommerceClientes.perfil", [
 	"ui.router"
 ]);
+
+$('#myTab a').click(function (e) {
+    debugger;
+  e.preventDefault()
+  $(this).tab('show')
+})

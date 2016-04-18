70ab5d1e10011c196d909ed37aac1052fd02d9ba
anaComercia <ana.comercia@gmail.com>
2016-04-18 17:13:29 -0300

    commit Ana
    se hace la pagina de perfil Reputacion

--- appClientes/app/perfil/PerfilController.js
+++ appClientes/app/perfil/PerfilController.js
@@ -1,5 +1,15 @@
-var perfil = angular.module("backendEcommerceClientes.perfil");
+angular
+    .module("backendEcommerceClientes.perfil")
+    .controller("PerfilController", PerfilCtrl);
 
-perfil.controller("PerfilController", function($state){
+PerfilCtrl.$injector = ["$state"];
+
+function PerfilCtrl($state) {
+    vm = this;
     
-});
+    vm.reputacionCliente = 75;
+    vm.mensajeReputacion = "Tu reputación es de ";
+    
+    vm.mensajeInformativo= "La reputación es un acumulador de puntos que comenzará con un valor de 100 al crearse la cuenta."
+                         + "Ésta disminuirá en 25 cada vez que usted realice una compra y no la concrete. Cuando la reputación llegue a 0 (cero), su cuenta será inhabilitada y deberá comunicarse con una sucursal para aclarar su situación; Del mismo modo, cada vez que usted realice una compra satisfactoria la reputación incrementará en 25 hasta un máximo de 100. "
+}

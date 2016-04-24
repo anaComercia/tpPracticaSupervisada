5bef624f234b349c01d3479f9810134c299c6dcf
anaComercia <ana.comercia@gmail.com>
2016-04-24 15:19:40 -0300

    commit Ana
    pagina de pago de productos

---
+++ appClientes/app/carrito/CarritoReservasController.js
@@ -0,0 +1,27 @@
+angular
+    .module("backendEcommerceClientes.carrito")
+    .controller("CarritoReservasController", CarritoReservasCtrl);
+
+CarritoReservasCtrl.$injector = ["$state , $scope"];
+
+function CarritoReservasCtrl($state,$scope) {
+   var vm = this;
+    debugger;
+    /* lista reservas */
+   vm.listaReservas = [
+        {
+            id:1,
+            image: 'img/modulos/prueba2.jpg', 
+            detail: 'campera milano', 
+            quantity: 1,
+            unitPrice: 800
+        },
+        {
+            id:1,
+            image: 'img/modulos/prueba2.jpg', 
+            detail: 'campera milano', 
+            quantity: 2,
+            unitPrice: 550
+        }
+    ];
+}
--- appClientes/app/carrito/PagarCompraController.js
+++ appClientes/app/carrito/PagarCompraController.js
@@ -8,5 +8,42 @@ function PagarCompraController($state,$scope) {
    var vm = this;
     
     vm.sumaTotalReservas = $scope.carrito.totalReservas;
-debugger;
+    
+    vm.mySelect = {};//seleccion de los combos
+      vm.tarjetas = [
+        {
+            id: 1,
+            type: 'VISA'
+        },
+        {
+            id: 2,
+            type: 'MASTER CARD'
+        },
+        {
+            id: 3,
+            type: 'AMEX'
+        }];
+      vm.cuotas = [1,2,3];
+    
+      vm.domicilios = [
+        {
+            id: 1,
+            detail: 'Ameguino 125'
+        },
+        {
+            id: 2,
+            detail: 'Segurola 1765'
+        },
+        {
+            id: 3,
+            detail: 'Jonte 987'
+        }];
+    
+  /*  $(function () {
+     $("#datepicker").datepicker({ 
+            autoclose: true, 
+            todayHighlight: true
+      }).datepicker('update', new Date());;
+    });*/
+
 }
